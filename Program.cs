e415707c87213eaa6e4d508e58830d763bacbb44
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-04-02 21:36:51 -0300

    Troca de variaveis

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿double milha;
+﻿int milha;
 double KM;
 
 Console.WriteLine("BEm-vindo ao contador conversor de milhas em KM");
 Console.WriteLine("Por favor digite a medida em milhas a ser convertido para KM");
-milha = Convert.ToDouble(Console.ReadLine());
+milha = Convert.ToInt32(Console.ReadLine());
 
-KM = Convert.ToDouble(milha * 1.609) ; 
+KM = milha * 1.609 ; 
 
 Console.WriteLine($"as milhas digitadas foram: {milha}, a conversão para KM: {KM} ");
