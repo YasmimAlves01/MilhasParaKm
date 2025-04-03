0d98503e84ce243db633e7545e4e112f7a4fa4ac
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-04-02 21:33:22 -0300

    Convertendo milhas para KM de forma simples

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double milha;
+double KM;
+
+Console.WriteLine("BEm-vindo ao contador conversor de milhas em KM");
+Console.WriteLine("Por favor digite a medida em milhas a ser convertido para KM");
+milha = Convert.ToDouble(Console.ReadLine());
+
+KM = Convert.ToDouble(milha * 1.609) ; 
+
+Console.WriteLine($"as milhas digitadas foram: {milha}, a conversão para KM: {KM} ");
